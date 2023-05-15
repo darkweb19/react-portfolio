81f99234d5f4c10c03109c1fd92e98c3821beff4
darkweb19 <sujanstha3220@gmail.com>
2023-05-15 16:55:53 +0545

    share ios icon is added to Carousel.js and statement's cards designs are tweeked a little

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,19 +4,22 @@ import { Carousel } from "react-responsive-carousel";
 import { v4 } from "uuid";
 import dallE from "../images/Dall-E.png";
 import ssrPng from "../images/SSR-Blog.png";
+import image from "../images/image.png";
 import minePort from "../images/portfolio.png";
 import Socials from "./Socials";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import IosShareIcon from "@mui/icons-material/IosShare";
 
 function Carousels() {
 	const uuid = () => v4;
 	const images = [
 		{
 			id: uuid(),
-			image: minePort,
-			name: "Website using pure html css and js",
+			image: image,
+			name: "A minimal Image Portfolio",
 			desc: " ",
-			link: "https://github.com/darkweb19/Portfolio-Website",
+			link: "https://github.com/darkweb19/image",
+			share: "https://darkweb19.github.io/image/imageGalary/",
 		},
 		{
 			id: uuid(),
@@ -24,6 +27,7 @@ function Carousels() {
 			name: "Image generator using Dall-E Api",
 			desc: " ",
 			link: "https://github.com/darkweb19/",
+			share: "",
 		},
 		{
 			id: uuid(),
@@ -31,6 +35,15 @@ function Carousels() {
 			name: "A blog using server side rendering",
 			desc: " ",
 			link: "https://github.com/darkweb19/SSR-Project1",
+			share: "",
+		},
+		{
+			id: uuid(),
+			image: minePort,
+			name: "Website using pure html css and js",
+			desc: " ",
+			link: "https://github.com/darkweb19/Portfolio-Website",
+			share: "https://github.com/darkweb19/Portfolio-Website",
 		},
 	];
 	return (
@@ -43,7 +56,7 @@ function Carousels() {
 			{images.map((item) => (
 				<div className="carousel-items" key={item.id}>
 					<img src={item.image} alt="nothing" />
-					<p>
+					<p className="para">
 						<Socials
 							className="social-links carousel"
 							social={
@@ -54,7 +67,17 @@ function Carousels() {
 							}
 							link={item.link}
 						/>
-						{item.name}
+						<Socials
+							className="social-links carousel"
+							social={
+								<IosShareIcon
+									sx={{ fontSize: 30 }}
+									color="primary"
+								/>
+							}
+							link={item.share}
+						/>
+						<p className="name">{item.name}</p>
 					</p>
 				</div>
 			))}
